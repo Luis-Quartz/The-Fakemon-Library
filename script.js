6d0cd9e8c3804e4c926639e73b799cd6a3ddc274
Luis Quartz <111935782+Luis-Quartz@users.noreply.github.com>
2024-02-26 23:51:10 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ function buildPokedex(currentUniverse) {
 		typeRows[i].setAttribute("onclick", functionString);
 		dexSprites[i].src = "Sprites/" + dexLinks[i].innerText + ".png";
 	}
+	handleforms();
 }
 
 //Originally developed for the HANDY910is model of Pokédex, this simple upgrade incorporates the functionality to handle multiple Pokémon forms as well as gender differences.
@@ -141,13 +142,8 @@ function flipSprite(spr, front, back) {
 }
 
 function distortReality() {
-	var thoughts = Array(
+	var fallerThoughts = Array(
 		"Hahaha, I guess not. That white hand on your shoulder... I'm just imagining it.",
-		"Our words shall remain here for the ages.",
-		"Stay... Won't you stay with me?",
-		"There is no one but you here...",
-		"Every rumor has a kernel of truth to it.",
-		"A flipped coin doesn't always land heads or tails. Sometimes it may never land at all...",
 		"By the way, who is that standing behind you?",
 		"Dad, Mom, Abra... Where are you...?",
 		"No, you're not the one...",
@@ -155,26 +151,19 @@ function distortReality() {
 		"Where... Where am I?",
 		"When I think about it, you, too, are all alone in the world.",
 		"Bold, precise, experimental.",
-		"Hello, starlight, you certainly gave everyone a scare.",
+		"Hello, starlight... You certainly gave everyone a scare.",
 		"Counting the seconds, standing alone, as thousands of years go by...",
 		"Don't become his lunch.",
 		"Shhh! Please walk quietly in the hallway!",
-		"L is real 2401.",
 		"We need to talk about parallel universes.",
 		"This message should not appear. I'll be scared if it does...",
-		"XD3R-B8HH-9ZR2-FL16",
-		"It's a secret to everybody.",
-		"I am Error.",
 		"You've met with a terrible fate, haven't you?",
 		"I think you are lost. It’s got to be around here somewhere...",
 		"Have you ever thought of a world where everything is exactly the same... except you don't exist?",
 		"Please don't think about this anymore.",
 		"Yes, your friend... The one behind you, with the creepy smile.",
 		"I just wasn't ready for the responsibility.",
-		"The air crackles with freedom.",
-		"It feels like a whirlwind.",
 		"It pulls the strings and makes them ring.",
-		"You lost it when you tried to see too far...",
 		"Can anyone hear me? Help...",
 		"It's dark... It's so dark here...",
 		"It could not be.",
@@ -187,8 +176,8 @@ function distortReality() {
 		"God save you if you hear something wandering around nearby, because it sure as hell has heard you.",
 		"This is the dimension of imagination."
 	)
-	var fun = Math.floor(Math.random()*thoughts.length);
-	echo = thoughts[fun];
+	var fun = Math.floor(Math.random()*fallerThoughts.length);
+	echo = fallerThoughts[fun];
 	abyss = document.getElementsByClassName("abyss");
 	abyss[0].innerText = echo;
 	pid = Math.floor(Math.random() * 66) + 1
